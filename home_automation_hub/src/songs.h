a88dc83fd516e7a20579b544ca87ea4c2b5e42e4
sz-fyr <z5530993@ad.unsw.edu.au>
2026-08-12 21:57:22 +1000

    Update songs.h

--- home_automation_hub/src/songs.h
+++ home_automation_hub/src/songs.h
@@ -1,7 +1,15 @@
-//Note datatype
+#ifndef SONGS_H
+#define SONGS_H
+
+// Note datatype
 struct tone {
-	int duration;
-	int pitch;
-	int volume;
+    int duration;
+    int pitch;
+    int volume;
 };
 
+// Song data stored in songs.c
+extern struct tone song_data[];
+extern int song_duration;
+
+#endif
