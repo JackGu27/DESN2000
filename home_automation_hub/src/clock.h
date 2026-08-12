cfd69beb2a103ce316d320dfb1a48673105b57e3
sz-fyr <z5530993@ad.unsw.edu.au>
2026-08-12 21:37:43 +1000

    Add function prototypes for clock management

--- home_automation_hub/src/clock.h
+++ home_automation_hub/src/clock.h
@@ -1,9 +1,18 @@
 #ifndef CLOCK_H
 #define CLOCK_H
 
+// Start the software clock
 void clock_init(void);
+
+// Update the current time
 void clock_update(void);
 
+// Set the current time
+void clock_set_time(unsigned int h,
+                    unsigned int m,
+                    unsigned int s);
+
+// Get the current time
 unsigned int clock_get_hour(void);
 unsigned int clock_get_minute(void);
 unsigned int clock_get_second(void);
