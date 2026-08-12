b869b428b6cc9bfd26ce7540058f0a1faf77f9a8
sz-fyr <z5530993@ad.unsw.edu.au>
2026-08-12 21:18:49 +1000

    Update clock.h

--- home_automation_hub/src/clock.h
+++ home_automation_hub/src/clock.h
@@ -1,7 +1,11 @@
-//Note datatype
-struct tone {
-	int duration;
-	int pitch;
-	int volume;
-};
+#ifndef CLOCK_H
+#define CLOCK_H
 
+void clock_init(void);
+void clock_update(void);
+
+unsigned int clock_get_hour(void);
+unsigned int clock_get_minute(void);
+unsigned int clock_get_second(void);
+
+#endif
