bd5aef63e8531e750b078ff907afed3af2196219
Zhengxi-Lu292 <z5594292@student.unsw.edu.au>
2026-08-13 14:40:44 +1000

    Update automation.c

--- home_automation_hub/src/automation.c
+++ home_automation_hub/src/automation.c
@@ -4,4 +4,20 @@
 extern void blind_set(int which, int position);
 extern unsigned int clock_now(void);
 
+// Shared automation state
+AutomationState automation_state;
+
+// Set safe/default values when the system starts.
+void automation_init(void)
+{
+    automation_state.light = 0;
+
+    automation_state.time = 0;
+
+    automation_state.blind1_state = UP;
+
+    automation_state.blind2_state = UP;
+
+    automation_state.plug_state = PLUG_OFF;
+}
 
