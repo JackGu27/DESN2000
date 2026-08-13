2c62da4b7c839469ea612e29a3070d6a1fedcfcb
Zhengxi-Lu292 <z5594292@student.unsw.edu.au>
2026-08-13 15:01:01 +1000

    Update automation.c

--- home_automation_hub/src/automation.c
+++ home_automation_hub/src/automation.c
@@ -17,14 +17,7 @@ void automation_init(void)
     automation_state.plug_state = PLUG_OFF;
 }
 
- // Called once during every main loop.
- * 2. Read current time
- * 3. Call decision.s
- * 4. Decode result
- * 5. Store blind and plug states
- * 6. Send blind state to S1
- */
-
+// Called once during every main loop.
 void automation_update(unsigned int light)
 {
     int result;
@@ -34,9 +27,26 @@ void automation_update(unsigned int light)
 // Store light value (input is light)
 automation_state.light = light;
 
+// Read current time
+automation_state.time = clock_now();
 
+// Call decision.s and ARM Assembly decision logic.
+result = decision_asm(automation_state.light, automation_state.time);
 
+// Decode result
+// Lowest two bits are about blind, 00 = DOWN; 01 = MID; 10 = UP
+blind = result & 0x3;
 
+// Extract smart plug state, Bit 2 contains 0 = OFF, 1 = ON
+plug = (result >> 2) & 0x1;
 
+// Store blind and plug states
+// Both blinds currently follow the same light decision.
+automation_state.blind1_state = blind;
+automation_state.blind2_state = blind;
+automation_state.plug_state = plug;
 
+// Send blind state to S1
+blind_set(BLIND_1, automation_state.blind1_state);
+blind_set(BLIND_2, automation_state.blind2_state);
 
