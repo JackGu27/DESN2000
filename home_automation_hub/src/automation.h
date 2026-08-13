18ee046873aaa16fe3575e88003ef9e51c433d8c
Zhengxi-Lu292 <z5594292@student.unsw.edu.au>
2026-08-13 14:18:23 +1000

    Update automation.h

--- home_automation_hub/src/automation.h
+++ home_automation_hub/src/automation.h
@@ -6,3 +6,11 @@
 #define DOWN  0
 #define MID   1
 #define UP    2
+
+// Two blinds in the cottage
+#define BLIND_1  0
+#define BLIND_2  1
+
+// Smart plug states
+#define PLUG_OFF  0
+#define PLUG_ON   1
