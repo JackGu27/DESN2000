009b09d430a581aeefde7017e108f045d6d99eb6
Zhengxi-Lu292 <z5594292@student.unsw.edu.au>
2026-08-13 14:32:01 +1000

    Update automation.h

--- home_automation_hub/src/automation.h
+++ home_automation_hub/src/automation.h
@@ -14,3 +14,35 @@
 // Smart plug states
 #define PLUG_OFF  0
 #define PLUG_ON   1
+
+// Shared automation state
+// This stores the latest automation result
+typedef struct {
+
+    unsigned int light;
+    unsigned int time;
+
+    int blind1_state;
+    int blind2_state;
+
+    int plug_state;
+
+} AutomationState;
+
+// Global automation state
+extern AutomationState automation_state;
+
+// light = ADC light sensor reading
+// time  = minutes after midnight
+extern int decision_asm(
+    unsigned int light,
+    unsigned int time
+);
+
+// Initialise automation system
+void automation_init(void);
+
+// Run one automation cycle
+void automation_update(unsigned int light);
+
+#endif
